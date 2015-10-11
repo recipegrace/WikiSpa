2d792e920af8dba0d5b3061cd4975dbe992bfedf
feroshjacob <feroshjacob@gmail.com>
2015-10-11 11:30:59 -0400

    configuration changes and removing unnecassary folders and files

--- spark/src/main/scala/com/recipegrace/wikispa/spark/CategoryCount.scala
+++ spark/src/main/scala/com/recipegrace/wikispa/spark/CategoryCount.scala
@@ -16,7 +16,7 @@ object CategoryCount {
     "org.apache.hadoop.streaming.StreamXmlRecordReader")
   jobConf.set("stream.recordreader.begin", "<page")
   jobConf.set("stream.recordreader.end", "</page>")
-  org.apache.hadoop.mapred.FileInputFormat.addInputPaths(jobConf, "files/enwiki-sample.xml")
+  org.apache.hadoop.mapred.FileInputFormat.addInputPaths(jobConf, "/Users/associate/wiki/enwiki-20151002-pages-articles-multistream.xml")
 
   // Load documents (one per line).
   val conf = new SparkConf().setAppName("Simple Application")
@@ -26,15 +26,18 @@ object CategoryCount {
     classOf[org.apache.hadoop.io.Text],
     classOf[org.apache.hadoop.io.Text])
 
-    val texts = documents.map(_._1.toString)
+    documents.map(_._1.toString)
       .map{ s =>
         val xml = XML.loadString(s)
-        Categories.extract(xml)
+        val modifiedXML = <mediawiki>{xml}</mediawiki>
+        Categories.extract(modifiedXML).getOrElse((0L,List():List[String]))
+
       }
-    .filter(f=> f.nonEmpty)
-    .flatMap(f=> f.get._2.map(f=> (f,1)))
-    .reduceByKey(_+_)
-    println(documents)
+    .filter(f=> f._1!=0)
+    .flatMap(f=> f._2.map(g=>(g,1)))
+    .aggregateByKey(0)((p,q)=> p+1, _+_)
+    .map(f=> f._2+"\t"+ f._1)
+    .saveAsTextFile(".tests/out2")
 
   }
 
--- spark/src/test/scala/com/recipegrace/wikispa/spark/CategoryCountTest.scala
+++ spark/src/test/scala/com/recipegrace/wikispa/spark/CategoryCountTest.scala
@@ -1,8 +1,24 @@
 package com.recipegrace.wikispa.spark
 
+import com.recipegrace.biglibrary.electric.ElectricJobTest
+
 /**
  * Created by Ferosh Jacob on 10/10/15.
  */
-class CategoryCountTest {
+class CategoryCountTest extends ElectricJobTest {
+
+  test("wordcount test with spark") {
+/*
+    val input = createOutPutFile()
+    createFile("hello world", input)
+    val output = createOutPutFile(false)
+    //launch(CategoryCount, Map("input" -> input, "output" -> output))
+
+    val lines = readFilesInDirectory(output, "part")
+    lines should contain("hello\t1")
+    lines should contain("world\t1")
+  */
+    CategoryCount.main(Array())
+  }
 
 }
