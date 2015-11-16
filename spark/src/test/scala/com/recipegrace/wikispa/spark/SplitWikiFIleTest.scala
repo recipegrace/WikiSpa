e5fab5f7f6ef5d4595dce68df36fe7ad4bad7ded
feroshjacob <feroshjacob@gmail.com>
2015-11-16 10:13:13 -0500

    fixed typo

---
+++ spark/src/test/scala/com/recipegrace/wikispa/spark/SplitWikiFIleTest.scala
@@ -0,0 +1,21 @@
+package com.recipegrace.wikispa.spark
+
+import java.nio.charset.StandardCharsets
+
+import com.recipegrace.biglibrary.electric.tests.TwoInputJobTest
+import com.recipegrace.biglibrary.electric.jobs.ThreeArgument
+
+/**
+ * Created by Ferosh Jacob on 10/25/15.
+ */
+class SplitWikiFileTest extends TwoInputJobTest {
+
+  test("check split file") {
+    val output = createTempPath()
+    launch(SplitWikiFile, ThreeArgument("files/enwiki-sample.xml",SplitWikiFile.SerializationType.SequenceFile.toString,output))
+
+    val lines = readFilesInDirectory(output, "part", StandardCharsets.ISO_8859_1)
+     lines should have size(9698)
+  }
+
+}
