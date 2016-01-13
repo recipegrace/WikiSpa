f6968b65a51569958f1a55001dd5991c860d7d07
feroshjacob <feroshjacob@gmail.com>
2016-01-12 21:37:48 -0500

    added snapshot of wikipedia extraction framework

--- project/Resolvers.scala
+++ project/Resolvers.scala
@@ -3,6 +3,7 @@ import sbt._
 object Resolvers {
   val mvnrepository = "MVN Repo" at "http://mvnrepository.com/artifact"
   val recipegrace = "Recipegrace repo" at "http://recipegrace.com:8080/nexus/content/repositories/releases/"
+  val recipegraceSnapshots = "Recipegrace snapshots" at "http://recipegrace.com:8080/nexus/content/repositories/snapshots/"
   val aksw="AKSW Maven2 Repository" at "http://maven.aksw.org/repository/internal"
   val cloudera = "Cloudera" at "https://repository.cloudera.com/artifactory/cloudera-repos"
   val allResolvers = Seq(Resolver.mavenLocal,mvnrepository,aksw,recipegrace)
