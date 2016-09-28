8b0aa8f765a83d9a90484bd7a8ab3d52dbb68aab
feroshjacob <feroshjacob@gmail.com>
2016-09-28 10:32:13 -0400

    removed central

--- project/Resolvers.scala
+++ project/Resolvers.scala
@@ -3,10 +3,9 @@ import sbt._
 
 object Resolvers {
   val ossReleases =  "Sonatype OSS Releases" at "https://oss.sonatype.org/content/repositories/releases/"
-  val ossCentral =  "Sonatype OSS Releases" at "https://oss.sonatype.org/content/repositories/central/"
   val ossSnapshots =  "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
   val ossStaging =  "Sonatype OSS Staging" at "https://oss.sonatype.org/service/local/staging/deploy/maven2"
 
-  val allResolvers = Seq(ossSnapshots,ossStaging,ossReleases, ossCentral)
+  val allResolvers = Seq(ossSnapshots,ossStaging,ossReleases)
 
 }
